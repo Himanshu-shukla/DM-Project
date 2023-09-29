ad3404c4a9bfa0c89a6733109a31bf8b33f66628
Suraj Kumar Jaiswal <graphicspapa@gmail.com>
2023-09-29 18:41:56 +0530

    alll changes left

--- public/client.js
+++ public/client.js
@@ -1,27 +1,48 @@
 document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    const name = document.getElementById('inputName').value;
-    const contact = document.getElementById('InputNumber').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('InputLocation').value;
-    fetch('/sendEmail', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `name=${name}&contact=${contact}&email=${email}&message=${message}`,
+  e.preventDefault();
+
+  const inputNumberGroup = document.querySelector('.InputNumber-group');
+  const inputNameGroup = document.querySelector('.name-group');
+  const inputEmailGroup = document.querySelector('.email-group');
+  const inputLocationGroup = document.querySelector('.location-group');
+
+
+  const name = document.getElementById('inputName').value;
+  const contact = document.getElementById('InputNumber').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('InputLocation').value;
+
+  fetch('/sendEmail', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `name=${name}&contact=${contact}&email=${email}&message=${message}`,
+  })
+    .then((response) => {
+      if (response.ok) {
+        document.getElementById('InputNumber').value = ""
+        inputNumberGroup.querySelectorAll('.palceholder').forEach(element => {
+          element.style.display = 'block';
+        });
+
+        document.getElementById('inputName').value = ""
+        inputNameGroup.querySelectorAll('.palceholder').forEach(element => {
+          element.style.display = 'block';
+        });
+        document.getElementById('email').value = ""
+        inputEmailGroup.querySelectorAll('.palceholder').forEach(element => {
+          element.style.display = 'block';
+        });
+        document.getElementById('InputLocation').value = ""
+        inputLocationGroup.querySelectorAll('.palceholder').forEach(element => {
+          element.style.display = 'block';
+        });
+      } else {
+        alert('Error sending email');
+      }
     })
-      .then((response) => {
-        if (response.ok) {
-          alert('Thank you for contacting us! We are here to assist you. Please give us a moment, and we will get back to you shortly.');
-          // Clear the form or perform any other actions here
-        } else {
-          alert('Error sending email');
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  });
-  
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+});
--- server.js
+++ server.js
@@ -12,30 +12,38 @@ app.post('/sendEmail', (req, res) => {
   const { name, contact, email, message } = req.body;
 
   // Create an email transport using Nodemailer and your email provider's settings
-  console.log(name,contact,email,message);
+  console.log(name, contact, email, message);
+
+  // Get the current date and time
+  const currentDate = new Date();
+
+  // Format the date as "DD/MM/YY"
+  const formattedDate = new Intl.DateTimeFormat('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: '2-digit'
+  }).format(currentDate);
+
+  // Format the time as "Hr/Min"
+  const formattedTime = new Intl.DateTimeFormat('en-GB', {
+    hour: 'numeric',
+    minute: 'numeric'
+  }).format(currentDate);
+
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'email.digitazmedia@gmail.com',
-      pass: 'ucxj ccww zjor zumi',
+      pass: 'cffm fdsb dhhm ybwx',
     },
   });
-
-  // var transporter = nodemailer.createTransport({
-  //   host: "sandbox.smtp.mailtrap.io",
-  //   port: 2525,
-  //   auth: {
-  //     user: "1f47a491814768",
-  //     pass: "79292106daef79"
-  //   }
-  // });
 
   // Define the email content and template
   const mailOptions = {
     from: 'Digitaz Media <email.digitazmedia@gmail.com>',
     to: "email.digitazmedia@gmail.com",
     subject: 'Enquiry Details',
-    text: `Name: ${name}\nEmail: ${email}\nContact: ${contact}\nLocation: ${message}`,
+    text: `Name: ${name}\nEmail: ${email}\nContact: ${contact}\nLocation: ${message}\nDate: ${formattedDate}\nTime: ${formattedTime}`,
   };
 
   // Send the email
